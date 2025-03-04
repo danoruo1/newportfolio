c59cec12a8ed680b6eb11a5b9f1faa7e9babab79
danoruo1 <138059604+danoruo1@users.noreply.github.com>
2025-03-04 11:39:36 -0500

    m

--- new-portfolio/src/routee.js
+++ new-portfolio/src/routee.js
@@ -1,20 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import App from './App';  // Import your Home component
+import Experience from './experiences/App';  // Correctly import the Experiences component
 
-const NavExp = () => {
-  const navigate = useNavigate();
-
+function AppRoutes() {
   return (
-    <a
-      href="/experiences"
-      className="navbartext"
-      onClick={(e) => {
-        e.preventDefault(); // Prevent full page reload
-        navigate("/experiences"); // Navigate using React Router
-      }}
-    >
-      Experiences
-    </a>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/experiences" element={<Experience />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
-export default NavExp
+export default AppRoutes;
